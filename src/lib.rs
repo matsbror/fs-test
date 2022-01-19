e57eb1ebc046b91a2c6a386f01552ecacabf4d83
Mats Brorsson <mats@embeinnovation.com>
2022-01-19 16:11:57 +0100

    include the cargo config file

---
+++ src/lib.rs
@@ -0,0 +1,175 @@
+use std::{str, borrow::Borrow};
+use wasmbus_rpc::actor::prelude::*;
+use serde_json::json;
+use wasmcloud_interface_httpserver::{HttpRequest, HttpResponse, HttpServer, HttpServerReceiver};
+use wasmcloud_interface_logging::info;
+use wasmcloud_interface_blobstore::*;
+mod query_string;
+use query_string::parse_query_string;
+
+#[derive(Debug, Default, Actor, HealthResponder)]
+#[services(Actor, HttpServer)]
+struct FsTestActor {}
+
+/// Implementation of HttpServer trait methods
+#[async_trait]
+impl HttpServer for FsTestActor {
+
+    async fn handle_request(&self, ctx: &Context, req: &HttpRequest) -> RpcResult<HttpResponse> {
+
+        match req.method.as_ref() {
+            "GET" => self.handle_get(ctx, req).await,
+            "POST" => self.handle_post(ctx, req).await,
+            "PUT" => self.handle_put(ctx, req).await ,
+            _ =>  Ok(HttpResponse {
+                        body: json!({ "error": "cannot handle method" }).to_string().into_bytes(),
+                        status_code: 400,
+                        ..Default::default()
+                    })
+        }
+
+    }
+
+}
+
+#[async_trait]
+impl ActorReceive for FsTestActor {
+
+    /// Receives a file chunk from a blobstore. This must be called AFTER
+    /// the StartUpload operation.
+    /// It is recommended to keep chunks under 1MB to not exceed wasm memory allocation
+    async fn receive_chunk(&self, ctx: &Context, arg: &Chunk) -> RpcResult<()> {
+
+        info!("receive_chunk called");
+
+
+
+        Ok(())
+    }
+
+}
+
+impl FsTestActor {
+
+    async fn handle_get(&self, ctx: &Context, req: &HttpRequest) -> RpcResult<HttpResponse> {
+
+        info!("GET request");
+
+        info!("path: {:?}", req.path);
+        info!("queries: {:?}", req.query_string);
+        info!("body size: {}", req.body.len());
+
+        Ok(HttpResponse {
+            body: "Success!".to_string().into_bytes(),
+            status_code: 200,
+            ..Default::default()
+        })
+    }
+
+    async fn handle_post(&self, ctx: &Context, req: &HttpRequest) -> RpcResult<HttpResponse> {
+
+        info!("POST request");
+
+        info!("path: {:?}", req.path);
+        info!("queries: {:?}", req.query_string);
+        info!("body size: {}", req.body.len());
+
+        let query_map = parse_query_string(&req.query_string);
+
+        let container_name = if query_map.contains_key("container") {
+            query_map["container"].clone()
+        } else {
+            "container".to_string()
+        };
+
+        let file_name = if query_map.contains_key("name") {
+            query_map["name"].clone()
+        } else {
+            "file.txt".to_string()
+        };
+
+        let bs_client = BlobstoreSender::new();
+
+        // create the container
+        let mut resp = bs_client.create_container(ctx, &container_name).await?;
+
+        if !resp.success {
+            return Ok(HttpResponse {
+                body: json!({ "error": resp.error }).to_string().into_bytes(),
+                status_code: 400,
+                ..Default::default()
+            });
+        }
+
+        let id = ObjectContId {container_id: container_name, object_id: file_name};
+        resp = bs_client.start_upload(ctx, &id).await?;
+
+        if !resp.success {
+            return Ok(HttpResponse {
+                body: json!({ "error": resp.error }).to_string().into_bytes(),
+                status_code: 400,
+                ..Default::default()
+            });
+        }
+
+        let c_size = 50;
+        let chunks = req.body.chunks(c_size);
+
+        let mut sequence_number = 0;
+        for chunk_body in chunks {
+            let chunk = Chunk {
+                ids: id.clone(),
+                bytes: chunk_body.to_vec().clone(),
+                chunk_size: c_size as u64,
+                context: None,
+                sequence_no: sequence_number,
+                total_bytes: req.body.len() as u64,
+            };
+
+            info!("actor call upload_chunk: {:?}", chunk);
+
+            resp = bs_client.upload_chunk(ctx, &chunk).await?;
+
+            if !resp.success {
+                return Ok(HttpResponse {
+                    body: json!({ "error": resp.error }).to_string().into_bytes(),
+                    status_code: 400,
+                    ..Default::default()
+                });
+            }
+
+            sequence_number += 1;
+        }
+
+
+        if !resp.success {
+            return Ok(HttpResponse {
+                body: json!({ "error": resp.error }).to_string().into_bytes(),
+                status_code: 400,
+                ..Default::default()
+            });
+        }
+
+        Ok(HttpResponse {
+            body: "Success!".to_string().into_bytes(),
+            status_code: 200,
+            ..Default::default()
+        })
+    }
+
+    async fn handle_put(&self, ctx: &Context, req: &HttpRequest) -> RpcResult<HttpResponse> {
+
+        info!("PUT request");
+
+        info!("path: {:?}", req.path);
+        info!("queries: {:?}", req.query_string);
+        info!("body size: {}", req.body.len());
+
+        Ok(HttpResponse {
+            body: "Success!".to_string().into_bytes(),
+            status_code: 200,
+            ..Default::default()
+        })
+    }
+    
+}
---
+++ src/query_string.rs
@@ -0,0 +1,98 @@
+use std::collections::HashMap;
+
+
+/// Utility for parsing query strings
+/// argument qs is a string of form key1=val1?key2=val2
+pub fn parse_query_string(qs: &str) -> HashMap<String, String> {
+    let mut map = HashMap::new();
+
+    if qs.len() > 0 {
+        let key_value_pairs = qs.split('?');
+
+        for kv_pair in key_value_pairs {
+            let kv: Vec<&str> = kv_pair.split('=').collect();
+
+            if kv.len() != 2 {
+                continue;
+            }
+
+            let key = kv[0];
+            let val = kv[1];
+            map.insert(key.to_string(), val.to_string());
+        }
+    }
+
+    map
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn zero_query() {
+        
+        let q1 = "";
+
+        let result = parse_query_string(q1);
+
+        assert_eq!(result.len(), 0);   
+    }
+
+    #[test]
+    fn no_equal_sign() {
+        
+        let q1 = "hello";
+
+        let result = parse_query_string(q1);
+
+        assert_eq!(result.len(), 0);   
+    }
+
+    #[test]
+    fn no_equal_sign_mixed() {
+        
+        let q1 = "hello?name=file";
+
+        let result = parse_query_string(q1);
+
+        assert_eq!(result.len(), 1);   
+
+        assert_eq!(result["name"], "file");
+    }
+ 
+    #[test]
+    fn one_query() {
+        
+        let q1 = "name=file.txt";
+
+        let result = parse_query_string(q1);
+
+        assert_eq!(result["name"], "file.txt");   
+    }
+
+    #[test]
+    fn two_queries() {
+        
+        let q1 = "name=file.txt?container=here_be_files";
+
+        let result = parse_query_string(q1);
+
+        assert_eq!(result["name"], "file.txt");   
+        assert_eq!(result["container"], "here_be_files");   
+    }
+
+    #[test]
+    fn three_queries() {
+        
+        let q1 = "name=file.txt?container=here_be_files?options=read_only";
+
+        let result = parse_query_string(q1);
+
+        assert_eq!(result["name"], "file.txt");   
+        assert_eq!(result["container"], "here_be_files");   
+        assert_eq!(result["options"], "read_only");   
+    }
+
+}
